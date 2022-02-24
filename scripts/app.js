10a50fa80b977d254608489a346154b57c00883c
Fahad-Faisaal <fahads1ab@gmail.com>
2022-02-24 22:02:12 +0600

    handler for learn more button added

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,25 @@
-
+// handler for search button
 const loadPlayer = async () => { 
   try {
+    // Clearing infos from previous learn more results
+    document.getElementById('player-details').classList.add('d-none');
+    document.getElementById('player-details').textContent='';
+
+    // clearing field and prev search
+    document.getElementById('players-container').style.display = 'block';
     const searchField = document.getElementById('search-input');
     const playerName = searchField.value;
     searchField.value = '';
     document.getElementById('players').textContent = '';
     
+    // fetching data to show search results
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${playerName}`;
-  
     const response = await fetch(url);
     const data = await response.json();
     const {player} = data
     displayPlayer(player);  
     
+    // removing prev error msg from ui
     document.querySelectorAll('.err-div').forEach(el => {
       el.classList.add('d-none');
     });
@@ -27,13 +34,15 @@ const loadPlayer = async () => {
     `;
 
     playersContainer.appendChild(div);
-  }
+  };
   
-}
+};
 
+// helping function for display results
 const displayPlayer = player => {
   const playersContainer = document.getElementById('players');
   
+  // generating result for each players
   player.forEach(player => {
     const playerDiv = document.createElement('div');
     playerDiv.classList.add('col');
@@ -56,21 +65,24 @@ const displayPlayer = player => {
   }); 
 }
 
+// handler for learn more button
 const displayDetail = async (id) => {
+  // look-up for player using id
   const url = `https://www.thesportsdb.com/api/v1/json/2/lookupplayer.php?id=${id}`;
-
   const response = await fetch(url);
   const data = await response.json();
+  const playerInfo = data.players[0];
    
+  // removing search results from ui
   document.getElementById('players-container').style.display = 'none';
 
+  // generating info results
   const playerDetailsContainer = document.getElementById('player-details');
-
   const div = document.createElement('div');
 
   div.innerHTML = `
-    <h1 class="text-center">Messi</h1>
-    <h4 class="text-center mb-5">Argentina</h4>
+    <h1 class="text-center">${playerInfo.strPlayer}</h1>
+    <h4 class="text-center mb-5">${playerInfo.strNationality}</h4>
     <table class="table table-dark table-striped w-75 mx-auto">
       <thead>
         <tr>
@@ -81,42 +93,47 @@ const displayDetail = async (id) => {
       <tbody>
         <tr>
           <th scope="row">Born</th>
-          <td>Mark</td>
+          <td>${playerInfo.dateBorn}</td>
         </tr>
         <tr>
           <th scope="row">Birth-place</th>
-          <td>dhaka</td>
+          <td>${playerInfo.strBirthLocation}</td>
         </tr>
         <tr>
           <th scope="row">Sports Name</th>
-          <td>cric</td>
+          <td>${playerInfo.strSport}</td>
         </tr>
         <tr>
           <th scope="row">Gender</th>
-          <td>male</td>
+          <td>${playerInfo.strGender}</td>
         </tr>
         <tr>
           <th scope="row">Position</th>
-          <td>forwd</td>
+          <td>${playerInfo.strPosition}</td>
         </tr>
         <tr>
           <th scope="row">Team</th>
-          <td>Chelsea</td>
+          <td>${playerInfo.strTeam}</td>
         </tr>
         <tr>
           <th scope="row">Height</th>
-          <td>6</td>
+          <td>${playerInfo.strHeight}</td>
         </tr>
         <tr>
           <th scope="row">Weight</th>
-          <td>66</td>
+          <td>${playerInfo.strWeight}</td>
         </tr>
         <tr>
           <th scope="row">Kit</th>
-          <td>Adidas</td>
+          <td>${playerInfo.strKit}</td>
         </tr>
       </tbody>
     </table>
   `;
+
+  playerDetailsContainer.appendChild(div);
+
+  //showing info on ui 
+  playerDetailsContainer.classList.remove('d-none');  
 }
 
